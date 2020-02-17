c28b10b963923925d624995ced457834d9236a1d
victor poirier <poiriervictor99@gmail.com>
2020-02-17 19:39:05 +0000

    added urls_show.ejs that shows the tiny url of an url

--- express_server.js
+++ express_server.js
@@ -7,8 +7,29 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.set("view engine", "ejs");
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
