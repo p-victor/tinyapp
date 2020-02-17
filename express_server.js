bb36f277c10a9f07d02f5bf3e3e04938d793aab0
victor poirier <poiriervictor99@gmail.com>
2020-02-17 20:21:31 +0000

    added newUrl view and also created a function to generate a random string (temporary test function)

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -7,16 +8,12 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//setting up the app
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
 
 app.get("/", (req, res) => {
   res.send("Hello!");
-});
-
-app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
 });
 
 app.get("/urls", (req, res) => {
@@ -24,14 +21,35 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+  const alphaNumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  const rng = Math.floor(Math.random() * 62);
+  let rndString = ""
+  for( let i = 0; i < 6; i++) { 
+    rndString += alphaNumeric[rng];
+  }
+  return rndString;
+}
